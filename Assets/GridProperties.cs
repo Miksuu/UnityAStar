77650c7590b3bc368e530036fbfb1498442ea841
Miksuu <imiksuu96@gmail.com>
2023-12-08 10:22:27 +0200

    Add gridproperties

--- Assets/GridProperties.cs
+++ Assets/GridProperties.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GridProperties : MonoBehaviour
 {
+    [SerializeField]
+    private int gridWidth;
+    [SerializeField]
+    private int gridHeight;
+    [SerializeField]
+    private int obstaclePercentage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
