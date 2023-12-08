5964cf9fbfbe38732a88f78cea7063e11087f9ee
Miksuu <imiksuu96@gmail.com>
2023-12-08 10:40:25 +0200

    Creation of the grid with obstacles

---
+++ Assets/Scripts/GridProperties.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridProperties : MonoBehaviour
+{
+    [SerializeField] private int gridWidth;
+    [SerializeField] private int gridHeight;
+    [SerializeField] private int obstaclePercentage;
+
+    [SerializeField] private GameObject gridPrefab;
+    [SerializeField] private GameObject obstaclePrefab;
+
+    private Grid[,] gridArray;
+
+    private void Start()
+    {
+        gridArray = new Grid[gridWidth, gridHeight];
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                GameObject toInstantiate = Random.Range(0, 100) < obstaclePercentage ? obstaclePrefab : gridPrefab;
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity);
+                gridArray[x, y] = instance.GetComponent<Grid>();
+                //gridArray[x, y].IsObstacle = toInstantiate == obstaclePrefab;
+                //gridArray[x, y].Coordinates = new Vector2(x, y);
+            }
+        }
+    }
+}
+
+public class Grid : MonoBehaviour
+{
+    public bool IsObstacle { get; set; }
+    public Vector2 Coordinates { get; set; }
+}
